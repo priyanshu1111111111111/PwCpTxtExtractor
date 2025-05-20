0e406a2161250d6f43838ae1da50c4eb4230d6c6
priyanshu1111111111111 <Priyanshu909090901@gmail.com>
2025-05-21 01:41:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "21155472"
 api_hash = "9eeb9128f391029ed536846341137e65"
-bot_token = "8117782805:AAEpKdCwgs5DjOFelhhdpTRdJpqsKdGwtBc"
+bot_token = "8036195069:AAG---vUEB4fG1vnRKOCb3B_aMGkNf_FhJE"
 auth_users = [6200095481]
